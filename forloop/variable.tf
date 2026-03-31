6b0bb279672dffd3116e678bfe917e803be99120
Venkat <psai96526@gmail.com>
2026-03-31 11:46:51 +0530

    imp

--- forloop/variable.tf
+++ forloop/variable.tf
@@ -1,5 +1,10 @@
 variable "instances" {
-    default = [ "mongodb", "redis", "mysql"]
+    default = {
+        mongodb ="t3.micro"
+        redis = "t3.micro"
+        mysql = "t3.micro"
+        rabbitmq = "t3.micro"
+    }
 }
 
 variable "zone_id" {
