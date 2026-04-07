4eef025e935b92eb5a65a714ec069595f396fb94
Venkat <psai96526@gmail.com>
2026-04-07 14:13:02 +0530

    imp

--- provisioners/ec2.tf
+++ provisioners/ec2.tf
@@ -14,6 +14,27 @@ resource "aws_instance" "terraform" {
     when    = destroy
     command = "echo 'Destroy'"
   }
+  
+  connection {
+    type = "ssh"
+    user = "ec2-user"
+    password = "DevOps321"
+    host = self.public_ip
+    }
+  
+  provisioner "remote-exec" {
+    inline = [
+      "sudo dnf install nginx -y",
+      "sudo systemctl start nginx",
+    ]
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo systemctl stop nginx",
+      "echo nginx is stopped"
+    ]
+    when = destroy
+  }
 }
 
 resource "aws_security_group" "allow-1" {
