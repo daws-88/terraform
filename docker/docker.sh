257fcab277cfd25598ef09dd50be04cb5ec1872b
Venkat <psai96526@gmail.com>
2026-06-18 19:27:32 +0530

    up

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
 ## clone dockerfile repo
 cd /home/ec2-user
 git clone https://github.com/daws-88/dockerfiles.git
+chown -R ec2-user:ec2-user dockerfiles
 
 ## increase disk volume
 growpart /dev/nvme0n1 4
