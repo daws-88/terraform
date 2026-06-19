05bf376d37f104cb00106d90332da6c3af1afe1d
Venkat <psai96526@gmail.com>
2026-06-19 20:50:21 +0530

    up

--- docker/docker.sh
+++ docker/docker.sh
@@ -2,7 +2,9 @@
 ## clone dockerfile repo
 cd /home/ec2-user
 git clone https://github.com/daws-88/dockerfiles.git
+git clone https://github.com/daws-88/docker-roboshop.git
 chown -R ec2-user:ec2-user dockerfiles
+chown -R ec2-user:ec2-user docker-roboshop
 
 ## increase disk volume
 growpart /dev/nvme0n1 4
