36514af4124b01c2f46e056641f4f09e7c474430
Venkat <psai96526@gmail.com>
2026-07-18 19:08:17 +0530

    up

--- docker/docker.sh
+++ docker/docker.sh
@@ -20,3 +20,17 @@ sudo systemctl enable --now docker
 sleep 10
 systemctl start docker
 usermod -aG docker ec2-user
+
+# install kubectl
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.35.3/2026-04-08/bin/linux/arm64/kubectl
+chmod +x ./kubectl
+sudo cp kubectl /usr/local/bin/
+
+## eksctl install
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+sudo install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
